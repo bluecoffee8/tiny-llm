05708dc188b0b5037b18e1d1e93c5d55e780e0a2
bluecoffee8 <jasperli2002@gmail.com>
2026-05-03 02:44:31 -0700

    day3

--- src/tiny_llm/qwen2_week1.py
+++ src/tiny_llm/qwen2_week1.py
@@ -24,15 +24,39 @@ class Qwen2MultiHeadAttention:
         max_seq_len: int = 32768,
         theta: int = 1000000,
     ):
-        pass
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads
+        self.head_dim = hidden_size // num_heads
+        self.scale = mx.rsqrt(self.head_dim)
+        self.wq = wq
+        self.wk = wk 
+        self.wv = wv
+        self.wo = wo 
+        self.bq = bq
+        self.bk = bk 
+        self.bv = bv 
+        self.max_seq_len = max_seq_len
+        self.theta = theta
+        self.rope = RoPE(self.head_dim, max_seq_len, theta, traditional=False)
 
     def __call__(
         self,
         x: mx.array,
         mask: mx.array | str | None = None,
     ) -> mx.array:
-        pass
-
+        B, L, _ = x.shape 
+        q = linear(x, self.wq, self.bq).reshape(B, L, self.num_heads, self.head_dim)
+        k = linear(x, self.wk, self.bk).reshape(B, L, self.num_kv_heads, self.head_dim)
+        v = linear(x, self.wv, self.bv).reshape(B, L, self.num_kv_heads, self.head_dim)
+        q = self.rope(q, offset=slice(0, L))
+        k = self.rope(k, offset=slice(0, L))
+        q = q.transpose(0, 2, 1, 3)
+        k = k.transpose(0, 2, 1, 3)
+        v = v.transpose(0, 2, 1, 3)
+        x = scaled_dot_product_attention_grouped(q.astype(mx.float32), k.astype(mx.float32), v.astype(mx.float32), scale=self.scale, mask=mask)
+        x = x.transpose(0, 2, 1, 3).reshape(B, L, -1)
+        return linear(x, self.wo) 
 
 class Qwen2MLP:
     def __init__(
